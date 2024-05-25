0957109c033105dbd0b8cce4f97a02f4e9d0bc85
An minsun <elleto123456@gmail.com>
2024-05-25 22:11:53 +0900

    초기 커밋.

---
+++ ch02/linkedList.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	struct node {
+		char data;
+		struct node* link;
+	};
+
+	char input = ' ';
+	struct node* head, * p;
+	head = (struct node*)malloc(sizeof(struct node));
+	p = head;
+	p->data = input;
+	p->link = NULL;
+	printf("데이터입력 : ");
+	scanf_s("%c", &input, 1);  //_s를 쓰면 데이터의 길이를 써야한다. => 1
+	getchar(); //문자 1개를 입력받기위해 사용하는 함수 // c에서는 데이터를 받고 enter값도 같이 받는다. 그래서 enter값을 없애주어야한다.
+
+	while (input != '0') {
+		p->link = (struct node*)malloc(sizeof(struct node));
+		p = p->link;
+		p->data = input;
+		p->link = NULL;
+		printf("데이터입력 : ");
+		scanf_s("%c", &input, 1);
+		getchar();
+	}
+	p = head->link; //별1
+
+	while (p != NULL) {
+		printf("%c", p->data);
+		if (p->link != NULL)
+			printf("-");
+		p = p->link;
+	}
+	printf("\n");
+}
---
+++ ch02/stackArray.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define SIZE 6
+
+int stack[SIZE];
+int top = -1;
+
+int push(int data) {
+	if (top == SIZE - 1) {
+		printf("stack is full!\n");
+		return -1;
+	}
+	stack[++top] = data;
+	return 0;
+}
+
+int pop(void) {
+	if (top == -1) {  //underflow√≥∏Æ
+		printf("stack is empty!\n");
+		return -1;
+	}
+	return stack[top--];
+}
+
+int main(void) {
+	push(10);
+	push(20);
+	push(30);
+	printf("%d\n", pop());  //30
+	printf("%d\n", pop());  //20
+	printf("%d\n", pop());  //10
+	printf("%d\n", pop());  //"stack is empty!" , -1
+	return 0;
+	
+}
